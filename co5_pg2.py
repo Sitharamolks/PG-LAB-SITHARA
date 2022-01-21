cdb2791f34862658b154907ceb509eca42425b34
SITHARA K S <93856172+Sitharamolks@users.noreply.github.com>
2022-01-21 10:56:22 +0530

    Delete co5_pg2.py

--- co5_pg2.py
+++
@@ -1,8 +0,0 @@
-f1 = open("firstfile.txt","r")
-for x in f1:
-    print(x)
-print("---------------")
-f1.seek(0,0)    
-ff=f1.readlines()
-print("Looping through the file using Readline\n")
-print("---------------")
